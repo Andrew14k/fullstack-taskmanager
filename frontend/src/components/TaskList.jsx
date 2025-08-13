3e184e5987239a49a11533b88fc678dee57e3e14
Andrew Knight <andrew.knight14wfc@gmail.com>
2025-08-13 11:42:20 +0100

    initial frontend setup

---
+++ frontend/src/components/TaskList.jsx
@@ -0,0 +1,22 @@
+const TaskList = () => {
+
+    const dummyData = [
+        {
+            "id": 1,
+            "taskName": "Setup React Frontend",
+            "description": "Follow tutorials to finish",
+            "deadline": "today",
+        },
+        {
+            "id": 2,
+            "taskName": "Setup React Frontend",
+            "description": "Follow videos to finish",
+            "deadline": "today",
+        }
+    ]
+    return (
+        <div>TaskList</div>
+    )
+}
+
+export default TaskList
